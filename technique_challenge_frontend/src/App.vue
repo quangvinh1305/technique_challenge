7958fdde0297588b206e69cb77380deb77de097d
Vinh Nguyen <quangvinh.1305@gmail.com>
2020-05-17 17:48:24 +0700

    fix

--- technique_challenge_frontend/src/App.vue
+++ technique_challenge_frontend/src/App.vue
@@ -7,22 +7,14 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt=" Logo"
           class="shrink mr-2"
           contain
           src="https://blog.firmsone.com/content/images/size/w100/2019/06/Free_Sample_By_Wix.jpeg"
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        Techinique Challenge
       </div>
 
       <v-spacer></v-spacer>
